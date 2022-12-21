5e3beaf22ef78edb617ac271a428a8db71ac4f75
laurin <laurin.lux@tuwien.ac.at>
2022-12-21 18:40:00 +0100

    separate classifier

---
+++ models/graph_classifier.py
@@ -0,0 +1,36 @@
+import torch
+
+
+class graphClassifierBatch():
+    def __init__(self, model, train_loader, test_loader, lr = 0.005, weight_decay = 5e-5):
+
+        self.model = model
+        self.optimizer= torch.optim.Adam(self.model.parameters(), lr= lr, weight_decay= weight_decay)
+        self.lossFunc = torch.nn.CrossEntropyLoss()
+
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+
+
+    def train(self):
+        self.model.train()
+        self.optimizer.zero_grad()  # Clear gradients.
+        for data in self.train_loader:
+            
+            out = self.model(data.x, data.edge_index, data.batch, training = True)  # Perform a single forward pass.
+            loss = self.lossFunc(out, data.y)  # Compute the loss solely based on the training nodes.
+            loss.backward()  # Derive gradients.
+            self.optimizer.step()  # Update parameters based on gradients.
+
+
+
+    @torch.no_grad()
+    def test(self):
+        self.model.eval()
+        correct = 0
+        for data in self.test_loader:  # Iterate in batches over the training/test dataset.
+            out = self.model(data.x, data.edge_index, data.batch, training = False)  
+            pred = out.argmax(dim=1)  # Use the class with highest probability.
+            correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+        return correct / len(self.test_loader.dataset)  # Derive ratio of correct predictions.
+
