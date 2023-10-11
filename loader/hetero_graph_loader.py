4ecb998ee23ed4088ed103757e4f74602299303b
Laurin <laurin.lux@tum.de>
2023-10-11 15:14:28 +0000

    update function for class labels and debug set

--- loader/hetero_graph_loader.py
+++ loader/hetero_graph_loader.py
@@ -39,12 +39,12 @@ class HeteroGraphLoaderTorch:
         self.hetero_edges_12 = self.read_hetero_edges(hetero_edges_path_12)
 
         if line_graph_1:
-            self.line_graphs_1 = self.line_graphs_alt(self.full_graphs_1)
+            self.line_graphs_1 = self.line_graphs(self.full_graphs_1)
         else:
             self.line_graphs_1 = None
 
         if line_graph_2:
-            self.line_graphs_2 = self.line_graphs_alt(self.full_graphs_2)
+            self.line_graphs_2 = self.line_graphs(self.full_graphs_2)
         else:
             self.line_graphs_2 = None
 
@@ -55,120 +55,22 @@ class HeteroGraphLoaderTorch:
         self.hetero_graphs = self.create_hetero_graphs(graphs_1, graphs_2)
         self.hetero_graph_list = list(self.hetero_graphs.values())
 
+    def update_class(self, class_dict):
 
+        UserWarning("Only hetero_graphs and hetero_graph_list are updated, not the full_graphs")
 
-    def create_hetero_graphs(self, g1 , g2):
+        self.octa_dr_dict = class_dict
 
-        het_graph_dict = {}
-        # iterate through the hetero edges
-
-        for key, het_edge_index in self.hetero_edges_12.items():
-            # get the two graphs
+        for i, (key, value) in enumerate(self.hetero_graphs.items()):
             try:
-                graph_1 = g1[key]
-                graph_2 = g2[key]
-            except KeyError:
-                continue
-            # create the hetero graph
-            het_graph = self.create_hetero_graph(graph_1, graph_2, het_edge_index)
-            het_graph_dict[key] = het_graph
-
-
-        return het_graph_dict
-
-    def create_hetero_graph(self, graph_1, graph_2, het_edge_index):
-            
-            # create the hetero graph
-            het_graph = HeteroData()
-    
-            het_graph['graph_1'].x = graph_1.x
-            het_graph['graph_2'].x = graph_2.x
-    
-            het_graph['graph_1'].pos = graph_1.pos
-            het_graph['graph_2'].pos = graph_2.pos
-
-            het_graph['graph_1', 'to', 'graph_1'].edge_index = graph_1.edge_index
-            het_graph['graph_2', 'to', 'graph_2'].edge_index = graph_2.edge_index
-            het_graph['graph_1', 'to', 'graph_2'].edge_index = het_edge_index
-
-            try:
-                het_graph.y = graph_1.y
+                disease = self.label_data[key]
+                cls = self.octa_dr_dict[disease]
+                value.y = torch.tensor([cls])
+                # update in list
+                self.hetero_graph_list[i].y = torch.tensor([cls])
             except KeyError:
                 pass
 
-            het_graph = ToUndirected()(het_graph)
-    
-            return het_graph
-
-    def read_hetero_edges(self, hetero_edges_path_12):
-
-        # iterate through all files in the folder
-        files = os.listdir(hetero_edges_path_12)
-        # use only the files with ending .csv
-        files = [file for file in files if ".csv" in file]
-        edge_dict = {}
-        for file in sorted(files):
-            idx = re.findall(r'\d+', str(file))[0]
-            if "_OD" in file:
-                eye = "OD"
-            else:
-                eye = "OS"
-            idx_dict = idx + "_" + eye
-            edge_df_ves_reg = pd.read_csv(os.path.join(
-                hetero_edges_path_12, file), sep=";", index_col=0)
-            
-            edge_dict[idx_dict] = torch.tensor(
-                                    edge_df_ves_reg[['node1id', 'node2id']].values, dtype=torch.long).t().contiguous()
-
-        return edge_dict
-
-
-    def create_torch_geom_data(self, node_df, edge_df, label=None):
-
-
-
-        node_features = torch.tensor(
-            node_df.values, dtype=torch.float32) # .loc[:, "pos_x":"isAtSampleBorder"]
-        node_pos = torch.tensor(
-            node_df.iloc[:, 0:2].values, dtype=torch.float32) # "pos_x":"pos_z"
-        edge_index = torch.tensor(
-            edge_df[['node1id', 'node2id']].values, dtype=torch.long).t().contiguous()
-
-        edge_attributes = torch.tensor(
-            edge_df.iloc[:, 2:].values, dtype=torch.float32)
-        
-        #if not self.load_center_nodes:
-#
-        #    # find the nodes with largest area (feature 2)
-        #    node_areas = node_features[:,2]
-        #    max_area_idx = node_areas.argmax()
-#
-        #    # remove the node from the node features
-        #    node_features = torch.cat((node_features[:max_area_idx], node_features[max_area_idx+1:]))
-        #    node_pos = torch.cat((node_pos[:max_area_idx], node_pos[max_area_idx+1:]))
-#
-        #    # remove the edges that contain the node
-        #    edge_index = edge_index[:, (edge_index[0] != max_area_idx) & (edge_index[1] != max_area_idx)]
-        #    edge_attributes = edge_attributes[(edge_index[0] != max_area_idx) & (edge_index[1] != max_area_idx)]
-#
-        #    # adjust the edge indices
-        #    edge_index[0, edge_index[0] > max_area_idx] -= 1
-        #    edge_index[1, edge_index[1] > max_area_idx] -= 1
-
-
-
-        # extracts for every edge its position which is the average of the two nodes
-
-        edge_pos = (node_pos[edge_index[0]] + node_pos[edge_index[1]])/2
-
-        if label is None:
-            data = Data(x=node_features, edge_index=edge_index,
-                        edge_attr=edge_attributes, pos=node_pos, edge_pos =edge_pos )
-        else:
-            label = torch.tensor(label)
-            data = Data(x=node_features, edge_index=edge_index,
-                        edge_attr=edge_attributes, pos=node_pos, edge_pos =edge_pos,  y=torch.tensor([label]))
-        return data
 
     def read_labels(self, label_file):
         label_data = pd.read_csv(label_file)
@@ -178,12 +80,16 @@ class HeteroGraphLoaderTorch:
         test, val = train_test_split(temp, test_size=0.5, random_state=42, stratify=temp["Group"])
         del temp
 
+        _, debug = train_test_split(label_data, test_size=0.05, random_state=42, stratify=label_data["Group"])
+
         if self.mode == "train":
             label_data = train            
         elif self.mode == "test":
             label_data = test
         elif self.mode == "val":
             label_data = val
+        elif self.mode == "debug":
+            label_data = debug
 
 
         label_dict = {}
@@ -255,8 +161,53 @@ class HeteroGraphLoaderTorch:
 
         return graph_dict
 
+    def create_torch_geom_data(self, node_df, edge_df, label=None):
+        node_features = torch.tensor(
+            node_df.values, dtype=torch.float32) # .loc[:, "pos_x":"isAtSampleBorder"]
+        node_pos = torch.tensor(
+            node_df.iloc[:, 0:2].values, dtype=torch.float32) # "pos_x":"pos_z"
+        edge_index = torch.tensor(
+            edge_df[['node1id', 'node2id']].values, dtype=torch.long).t().contiguous()
 
-    def line_graphs_alt(self, full_data):
+        edge_attributes = torch.tensor(
+            edge_df.iloc[:, 2:].values, dtype=torch.float32)
+        
+        # extracts for every edge its position which is the average of the two nodes
+
+        edge_pos = (node_pos[edge_index[0]] + node_pos[edge_index[1]])/2
+
+        if label is None:
+            data = Data(x=node_features, edge_index=edge_index,
+                        edge_attr=edge_attributes, pos=node_pos, edge_pos =edge_pos )
+        else:
+            label = torch.tensor(label)
+            data = Data(x=node_features, edge_index=edge_index,
+                        edge_attr=edge_attributes, pos=node_pos, edge_pos =edge_pos,  y=torch.tensor([label]))
+        return data
+
+    def read_hetero_edges(self, hetero_edges_path_12):
+
+        # iterate through all files in the folder
+        files = os.listdir(hetero_edges_path_12)
+        # use only the files with ending .csv
+        files = [file for file in files if ".csv" in file]
+        edge_dict = {}
+        for file in sorted(files):
+            idx = re.findall(r'\d+', str(file))[0]
+            if "_OD" in file:
+                eye = "OD"
+            else:
+                eye = "OS"
+            idx_dict = idx + "_" + eye
+            edge_df_ves_reg = pd.read_csv(os.path.join(
+                hetero_edges_path_12, file), sep=";", index_col=0)
+            
+            edge_dict[idx_dict] = torch.tensor(
+                                    edge_df_ves_reg[['node1id', 'node2id']].values, dtype=torch.long).t().contiguous()
+
+        return edge_dict
+
+    def line_graphs(self, full_data):
         line_graph_dict = {}
 
         for key, value in full_data.items():
@@ -306,6 +257,49 @@ class HeteroGraphLoaderTorch:
 
         return line_graph_dict
     
+
+    def create_hetero_graphs(self, g1 , g2):
+
+        het_graph_dict = {}
+        # iterate through the hetero edges
+
+        for key, het_edge_index in self.hetero_edges_12.items():
+            # get the two graphs
+            try:
+                graph_1 = g1[key]
+                graph_2 = g2[key]
+            except KeyError:
+                continue
+            # create the hetero graph
+            het_graph = self.create_hetero_graph(graph_1, graph_2, het_edge_index)
+            het_graph_dict[key] = het_graph
+
+
+        return het_graph_dict
+
+    def create_hetero_graph(self, graph_1, graph_2, het_edge_index):
+            
+            # create the hetero graph
+            het_graph = HeteroData()
+    
+            het_graph['graph_1'].x = graph_1.x
+            het_graph['graph_2'].x = graph_2.x
+    
+            het_graph['graph_1'].pos = graph_1.pos
+            het_graph['graph_2'].pos = graph_2.pos
+
+            het_graph['graph_1', 'to', 'graph_1'].edge_index = graph_1.edge_index
+            het_graph['graph_2', 'to', 'graph_2'].edge_index = graph_2.edge_index
+            het_graph['graph_1', 'to', 'graph_2'].edge_index = het_edge_index
+
+            try:
+                het_graph.y = graph_1.y
+            except KeyError:
+                pass
+
+            het_graph = ToUndirected()(het_graph)
+    
+            return het_graph
 
 
     def __len__(self):
