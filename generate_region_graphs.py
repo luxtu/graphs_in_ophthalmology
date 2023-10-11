91f07f3942ada4eea23031daa71493dac542b020
Laurin <laurin.lux@tum.de>
2023-10-11 15:16:42 +0000

    generation of DCP graphs

--- generate_region_graphs.py
+++ generate_region_graphs.py
@@ -1,10 +1,21 @@
 from generator import heterograph_from_vvg_generator
 
 
-Gernerator_from_VVG = heterograph_from_vvg_generator.HeterographFromVVGGenerator("/media/data/alex_johannes/octa_data/Cairo/SCP_seg",
-                                                              "/media/data/alex_johannes/octa_data/Cairo/SCP_json", 
-                                                              "/media/data/alex_johannes/octa_data/Cairo/SCP_void_graph", 
-                                                              "/media/data/alex_johannes/octa_data/Cairo/SCP_heter_edges",
-                                                              image_path= "/media/data/alex_johannes/octa_data/Cairo/SCP_images",
-                                                              debug=False)
+
+data_type = "DCP"
+
+segmentation_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_seg"
+json_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_json"
+void_graph_save_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_void_graph"
+hetero_edges_save_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_heter_edges"
+image_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_images"
+
+
+Gernerator_from_VVG = heterograph_from_vvg_generator.HeterographFromVVGGenerator(
+                                                            seg_path= segmentation_path,
+                                                            json_path= json_path,
+                                                            void_graph_save_path= void_graph_save_path,
+                                                            hetero_edges_save_path= hetero_edges_save_path,
+                                                            image_path= image_path,
+                                                            debug=False)
 Gernerator_from_VVG.save_region_graphs()
