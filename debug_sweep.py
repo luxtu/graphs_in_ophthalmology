e94a15ca7c67f2a7df6a68d93b34869b143863df
Laurin <laurin.lux@tum.de>
2023-10-25 17:41:49 +0000

    gnn for reg and cls

--- debug_sweep.py
+++ debug_sweep.py
@@ -4,9 +4,8 @@ import torch
 
 from sklearn.metrics import accuracy_score, balanced_accuracy_score
 from torch_geometric.nn import global_mean_pool, global_max_pool, global_add_pool
-from models import graph_classifier, hetero_gnn, global_node_gnn, hierarchical_gnn
+from models import graph_classifier, hetero_gnn, global_node_gnn, hierarchical_gnn, spatial_pooling_gnn
 from torch_geometric.loader import DataLoader
-from torch_geometric.transforms import Compose, RemoveIsolatedNodes
 
 from sklearn.metrics import roc_auc_score
 from utils import prep
@@ -23,14 +22,14 @@ my_obj = SimpleNamespace(**my_dict)
 # Define sweep config
 sweep_config_dict = {
         "batch_size": 4,
-        "epochs": 10,
+        "epochs": 100,
         "lr": 0.005,
         "weight_decay": 0.001,
         "hidden_channels": 32,
         "dropout":  0.1, 
         "num_layers":  2,
-        "aggregation_mode":  global_mean_pool,
-        "class_weights": "unbalanced", 
+        "aggregation_mode":  "global_mean_pool",
+        "class_weights": "balanced", 
         "dataset": "DCP"} 
 
 sweep_config = SimpleNamespace(**sweep_config_dict)
@@ -38,7 +37,7 @@ sweep_config = SimpleNamespace(**sweep_config_dict)
 
 data_type = sweep_config.dataset
 
-octa_dr_dict = {"Healthy": 0, "DM": 0, "PDR": 1, "Early NPDR": 2, "Late NPDR": 2}
+octa_dr_dict = {"Healthy": 0, "DM": 0, "PDR": 2, "Early NPDR": 1, "Late NPDR": 1}
 label_names = ["Healthy/DM", "PDR", "NPDR"]
 
 vessel_graph_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_vessel_graph"
@@ -62,38 +61,28 @@ test_dataset = hetero_graph_loader.HeteroGraphLoaderTorch(vessel_graph_path,
                                                         label_file = label_file, 
                                                         line_graph_1 =True, 
                                                         class_dict = octa_dr_dict)
-print(len(train_dataset))
 
-# remove isolated nodes
-for data in train_dataset:
-    RemoveIsolatedNodes()(data) 
-
-for data in test_dataset:
-    RemoveIsolatedNodes()(data) 
 
 # imputation and normalization
 prep.hetero_graph_imputation(train_dataset)
 prep.hetero_graph_imputation(test_dataset)
+
+prep.add_node_features(train_dataset, ["graph_1", "graph_2"])
+prep.add_node_features(test_dataset, ["graph_1", "graph_2"])
 
 prep.add_global_node(train_dataset)
 prep.add_global_node(test_dataset)
 
 
 #node_mean_tensors, node_std_tensors = prep.hetero_graph_normalization_params(train_dataset)
-node_mean_tensors = torch.load(f"checkpoints/{data_type}_node_mean_tensors_global_node.pt")
-node_std_tensors = torch.load(f"checkpoints/{data_type}_node_std_tensors_global_node.pt")
+node_mean_tensors = torch.load(f"checkpoints/{data_type}_node_mean_tensors_global_node_node_degs.pt")
+node_std_tensors = torch.load(f"checkpoints/{data_type}_node_std_tensors_global_node_node_degs.pt")
 
 prep.hetero_graph_normalization(train_dataset, node_mean_tensors, node_std_tensors)
 prep.hetero_graph_normalization(test_dataset, node_mean_tensors, node_std_tensors)
 
-node_nums = []
-for data in train_dataset:
-    node_nums.append(data.x_dict["graph_1"].shape[0] + data.x_dict["graph_2"].shape[0])
 
-for data in test_dataset:
-    node_nums.append(data.x_dict["graph_1"].shape[0] + data.x_dict["graph_2"].shape[0])
-
-max_nodes = np.max(node_nums)
+max_nodes = np.max([graph.num_nodes for graph in train_dataset] + [graph.num_nodes for graph in test_dataset])
 print(max_nodes)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -102,8 +91,13 @@ train_labels = [int(data.y[0]) for data in train_dataset]
 class_weights = prep.get_class_weights(train_labels, verbose=False)
 class_weights = torch.tensor(class_weights, device= device)
 
+print(class_weights)
+# make differences in weights smaller
+class_weights = class_weights ** 0.5
+print(class_weights)
 
-num_classes = 3
+
+num_classes = class_weights.shape[0]
 node_types = ["graph_1", "graph_2"]
 
 
@@ -114,13 +108,21 @@ agg_mode_dict = {"global_mean_pool": global_mean_pool, "global_max_pool": global
 def main():
     torch.autograd.set_detect_anomaly(True)
 
-    model = hierarchical_gnn.DiffPool_GNN(hidden_channels= sweep_config.hidden_channels,
-                                        out_channels= num_classes,
-                                        num_layers= sweep_config.num_layers,
-                                        dropout = sweep_config.dropout,
-                                        node_types = node_types,
-                                        max_nodes = max_nodes
-                                        )
+    #model = hierarchical_gnn.DiffPool_GNN(hidden_channels= sweep_config.hidden_channels,
+    #                                    out_channels= num_classes,
+    #                                    num_layers= sweep_config.num_layers,
+    #                                    dropout = sweep_config.dropout,
+    #                                    node_types = node_types,
+    #                                    max_nodes = max_nodes
+    #                                    )
+    
+    model = spatial_pooling_gnn.GeomPool_GNN(hidden_channels = sweep_config.hidden_channels, 
+                              out_channels= num_classes, 
+                              num_layers= sweep_config.num_layers, 
+                              dropout = sweep_config.dropout, 
+                              aggregation_mode= agg_mode_dict[sweep_config.aggregation_mode],
+                              node_types = node_types,
+                              )
 
     # create data loaders for training and test set
     train_loader = DataLoader(train_dataset, batch_size = sweep_config.batch_size, shuffle=True)
@@ -130,9 +132,11 @@ def main():
     balanced_loss = torch.nn.CrossEntropyLoss(class_weights)
     unbalanced_loss = torch.nn.CrossEntropyLoss()
 
+    reg_loss = torch.nn.SmoothL1Loss()
+
     loss_dict = {"balanced": balanced_loss, "unbalanced": unbalanced_loss}
 
-    classifier = graph_classifier.graphClassifierHetero(model, loss_dict[sweep_config.class_weights], lr = sweep_config.lr, weight_decay =sweep_config.weight_decay)
+    classifier = graph_classifier.graphClassifierHetero(model,reg_loss , lr = sweep_config.lr, weight_decay =sweep_config.weight_decay, regression=True) #loss_dict[sweep_config.class_weights]
 
     best_val_bal_acc = 0
     best_mean_auc = 0
@@ -140,11 +144,15 @@ def main():
     for epoch in range(1, sweep_config.epochs + 1):
         loss = classifier.train(train_loader)
         print(f"Epoch: {epoch:03d}")
-        print(torch.cuda.memory_summary())
-        train_acc = classifier.test(train_loader)
+
         outList, yList = classifier.predict(test_loader)
-        #print(outList)
-        y_p = np.array([item.argmax().cpu().detach().numpy() for sublist in outList for item in sublist])
+
+        reg = True
+        if not reg:
+            y_p = np.array([item.argmax().cpu().detach().numpy() for sublist in outList for item in sublist])
+        else:
+            y_p = np.array([item.cpu().detach().numpy() for sublist in outList for item in sublist])
+
         y_t = np.array([item.detach().cpu().numpy() for sublist in yList for item in sublist])
         #print(f'Epoch: {epoch:03d}, Loss: {loss:.6f}, Train Acc: {train_acc:.4f}, Test Acc: {accuracy_score(y_t, y_p):.4f}, Test Bal Acc: {balanced_accuracy_score(y_t, y_p):.4f}')
 
@@ -156,18 +164,28 @@ def main():
         if test_bal_acc > best_val_bal_acc:
             best_val_bal_acc = test_bal_acc
 
-        _, _, roc_auc = evaluation.roc_auc_multiclass(y_t, torch.nn.functional.softmax(torch.tensor(np.array(outList).squeeze()), dim = 1))
-        mean_auc = np.mean(list(roc_auc.values()))
+        #_, _, roc_auc = evaluation.roc_auc_multiclass(y_t, torch.nn.functional.softmax(torch.tensor(np.array(outList).squeeze()), dim = 1))
+        #mean_auc = np.mean(list(roc_auc.values()))
                            
-        if mean_auc > best_mean_auc or (mean_auc == best_mean_auc and test_bal_acc > best_val_bal_acc):
-            best_mean_auc = mean_auc
+        #if mean_auc > best_mean_auc or (mean_auc == best_mean_auc and test_bal_acc > best_val_bal_acc):
+        #    best_mean_auc = mean_auc
 
 
-        res_dict =  {"loss": loss, "train_acc": train_acc, "val_acc": test_acc, "val_bal_acc": test_bal_acc, "best_val_bal_acc": best_val_bal_acc,"mean_auc" : mean_auc,"best_mean_auc": best_mean_auc}
-        for i in range(num_classes):
-            res_dict[f"roc_auc_{label_names[i]}"] = roc_auc[i]
+        #res_dict =  {"loss": loss, "train_acc": train_acc, "val_acc": test_acc, "val_bal_acc": test_bal_acc, "best_val_bal_acc": best_val_bal_acc,"mean_auc" : mean_auc,"best_mean_auc": best_mean_auc}
+        #for i in range(num_classes):
+        #    res_dict[f"roc_auc_{label_names[i]}"] = roc_auc[i]
+
+        # y_p_softmax = torch.nn.functional.softmax(torch.tensor(np.array(outList).squeeze()), dim = 1).detach().numpy()
+        #print(y_p_softmax)
+        #macro_roc_auc_ovr = roc_auc_score(
+        #    y_true=y_t,
+        #    y_score = y_p_softmax[:,1],
+        #    multi_class="ovr",
+        #    average="macro",
+        #)
+        #print(macro_roc_auc_ovr)
 
 
-    print(roc_auc)
+    #print(roc_auc)
 
 main()
--- gnn_sweep.py
+++ gnn_sweep.py
@@ -2,16 +2,17 @@ from loader import hetero_graph_loader
 import numpy as np 
 import torch
 
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, cohen_kappa_score
 from torch_geometric.nn import global_mean_pool, global_max_pool, global_add_pool
-from models import graph_classifier, hetero_gnn, global_node_gnn, hierarchical_gnn
+from models import graph_classifier, hetero_gnn, global_node_gnn, hierarchical_gnn, spatial_pooling_gnn
 from torch_geometric.loader import DataLoader
-from torch_geometric.transforms import Compose, RemoveIsolatedNodes
 
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 from utils import prep
 from evaluation import evaluation
 import wandb
+from sklearn.preprocessing import LabelBinarizer
+
 
 torch.cuda.empty_cache()
 
@@ -21,16 +22,17 @@ sweep_configuration = {
     "name": "sweep",
     "metric": {"goal": "maximize", "name": "best_mean_auc"},
     "parameters": {
-        "batch_size": {"values": [2]},
-        "epochs": {"values": [50]},
+        "batch_size": {"values": [8, 16, 32, 64]},
+        "epochs": {"values": [100]},
         "lr": {"max": 0.05, "min": 0.005}, # learning rate to high does not work
         "weight_decay": {"max": 0.01, "min": 0.00001},
-        "hidden_channels": {"values": [16, 32]}, #[64, 128]
-        "dropout": {"values": [0.3, 0.4]}, # 0.2,  more droput looks better
+        "hidden_channels": {"values": [32, 64]}, #[64, 128]
+        "dropout": {"values": [0.1, 0.3, 0.4]}, # 0.2,  more droput looks better
         "num_layers": {"values": [1,2,3]},
-        "aggregation_mode": {"values": ["global_mean_pool", "global_max_pool"]},#, "global_mean_pool",  "global_add_pool" # add pool does not work
-        "class_weights": {"values": ["unbalanced"]}, # "balanced", 
-        "dataset": {"values": ["DCP"]} #, "DCP"
+        "aggregation_mode": {"values": ["global_mean_pool", "global_add_pool"]},#, "global_mean_pool",  "global_add_pool" # add pool does not work
+        "class_weights": {"values": ["unbalanced", "balanced"]}, # "balanced", 
+        "dataset": {"values": ["DCP"]}, #, "DCP"
+        "regression": {"values": [True, False]},
     },
 }
 
@@ -39,8 +41,8 @@ sweep_id = wandb.sweep(sweep=sweep_configuration, project="graph_pathology")
 
 data_type = sweep_configuration["parameters"]["dataset"]["values"][0]
 
-octa_dr_dict = {"Healthy": 0, "DM": 0, "PDR": 1, "Early NPDR": 2, "Late NPDR": 2}
-label_names = ["Healthy/DM", "PDR", "NPDR"]
+octa_dr_dict = {"Healthy": 0, "DM": 0, "PDR": 2, "Early NPDR": 1, "Late NPDR": 1}
+label_names = ["Healthy/DM", "NPDR", "PDR"]
 
 vessel_graph_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_vessel_graph"
 void_graph_path = f"/media/data/alex_johannes/octa_data/Cairo/{data_type}_void_graph"
@@ -64,48 +66,42 @@ test_dataset = hetero_graph_loader.HeteroGraphLoaderTorch(vessel_graph_path,
                                                         line_graph_1 =True, 
                                                         class_dict = octa_dr_dict)
 
-# remove isolated nodes
-for data in train_dataset:
-    RemoveIsolatedNodes()(data) 
-
-for data in test_dataset:
-    RemoveIsolatedNodes()(data) 
 
 # imputation and normalization
 prep.hetero_graph_imputation(train_dataset)
 prep.hetero_graph_imputation(test_dataset)
 
+prep.add_node_features(train_dataset, ["graph_1", "graph_2"])
+prep.add_node_features(test_dataset, ["graph_1", "graph_2"])
+
 prep.add_global_node(train_dataset)
 prep.add_global_node(test_dataset)
 
+node_mean_tensors, node_std_tensors = prep.hetero_graph_normalization_params(train_dataset)
+
+# save the normalization parameters
+#torch.save(node_mean_tensors, f"checkpoints/{data_type}_node_mean_tensors_global_node_node_degs.pt")
+#torch.save(node_std_tensors, f"checkpoints/{data_type}_node_std_tensors_global_node_node_degs.pt")
 
 #node_mean_tensors, node_std_tensors = prep.hetero_graph_normalization_params(train_dataset)
-node_mean_tensors = torch.load(f"checkpoints/{data_type}_node_mean_tensors_global_node.pt")
-node_std_tensors = torch.load(f"checkpoints/{data_type}_node_std_tensors_global_node.pt")
+node_mean_tensors = torch.load(f"checkpoints/{data_type}_node_mean_tensors_global_node_node_degs.pt")
+node_std_tensors = torch.load(f"checkpoints/{data_type}_node_std_tensors_global_node_node_degs.pt")
 
 prep.hetero_graph_normalization(train_dataset, node_mean_tensors, node_std_tensors)
 prep.hetero_graph_normalization(test_dataset, node_mean_tensors, node_std_tensors)
 
-node_nums = []
-for data in train_dataset:
-    node_nums.append(data.x_dict["graph_1"].shape[0] + data.x_dict["graph_2"].shape[0])
-
-for data in test_dataset:
-    node_nums.append(data.x_dict["graph_1"].shape[0] + data.x_dict["graph_2"].shape[0])
-
-max_nodes = np.max(node_nums) # probably need to take a closer look at the adj matrix creation
+max_nodes = np.max([graph.num_nodes for graph in train_dataset] + [graph.num_nodes for graph in test_dataset])
 print(max_nodes)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # class weight extraction
 train_labels = [int(data.y[0]) for data in train_dataset]
 class_weights = prep.get_class_weights(train_labels, verbose=False)
-class_weights = torch.tensor(class_weights, device= device)
+class_weights = torch.tensor(class_weights, device=device)
+class_weights = class_weights ** 0.5 
 
-
-num_classes = 3
+num_classes = class_weights.shape[0]
 node_types = ["graph_1", "graph_2"]
-
 
 agg_mode_dict = {"global_mean_pool": global_mean_pool, "global_max_pool": global_max_pool, "global_add_pool": global_add_pool}
 
@@ -126,23 +122,23 @@ def main():
     # create the model
     # gnn_models.HeteroGNN
     #global_node_gnn.GNN_global_node
-    model = hierarchical_gnn.DiffPool_GNN(hidden_channels= wandb.config.hidden_channels,
-                                        out_channels= num_classes,
-                                        num_layers= wandb.config.num_layers,
-                                        dropout = wandb.config.dropout,
-                                        node_types = node_types,
-                                        max_nodes = max_nodes
-                                        )
+    #model = hierarchical_gnn.DiffPool_GNN(hidden_channels= wandb.config.hidden_channels,
+    #                                    out_channels= num_classes,
+    #                                    num_layers= wandb.config.num_layers,
+    #                                    dropout = wandb.config.dropout,
+    #                                    node_types = node_types,
+    #                                    max_nodes = max_nodes
+    #                                    )
 
 
 
-    #model = global_node_gnn.GNN_global_node(hidden_channels = wandb.config.hidden_channels, 
-    #                          out_channels= num_classes, 
-    #                          num_layers= wandb.config.num_layers, 
-    #                          dropout = wandb.config.dropout, 
-    #                          aggregation_mode= agg_mode_dict[wandb.config.aggregation_mode],
-    #                          node_types = node_types,
-    #                          )
+    model = spatial_pooling_gnn.GeomPool_GNN(hidden_channels = wandb.config.hidden_channels, 
+                              out_channels= num_classes, 
+                              num_layers= wandb.config.num_layers, 
+                              dropout = wandb.config.dropout, 
+                              aggregation_mode= agg_mode_dict[wandb.config.aggregation_mode],
+                              node_types = node_types,
+                              )
 
     # create data loaders for training and test set
     train_loader = DataLoader(train_dataset, batch_size = wandb.config.batch_size, shuffle=True)
@@ -154,7 +150,11 @@ def main():
 
     loss_dict = {"balanced": balanced_loss, "unbalanced": unbalanced_loss}
 
-    classifier = graph_classifier.graphClassifierHetero(model, loss_dict[wandb.config.class_weights], lr = wandb.config.lr, weight_decay =wandb.config.weight_decay)
+    if wandb.config.regression:
+        reg_loss = torch.nn.SmoothL1Loss()
+        classifier = graph_classifier.graphClassifierHetero(model,reg_loss , lr = wandb.config.lr, weight_decay =wandb.config.weight_decay, regression=wandb.config.regression)
+    else:
+        classifier = graph_classifier.graphClassifierHetero(model, loss_dict[wandb.config.class_weights], lr = wandb.config.lr, weight_decay =wandb.config.weight_decay, regression=wandb.config.regression) 
 
     best_val_bal_acc = 0
     best_mean_auc = 0
@@ -163,42 +163,76 @@ def main():
         loss = classifier.train(train_loader)
         train_acc = classifier.test(train_loader)
         outList, yList = classifier.predict(test_loader)
-        y_p = np.array([item.argmax().cpu().detach().numpy() for sublist in outList for item in sublist])
+
+        if wandb.config.regression:
+            y_p = np.array([item.cpu().detach().numpy() for sublist in outList for item in sublist])
+        else:
+            y_p = np.array([item.argmax().cpu().detach().numpy() for sublist in outList for item in sublist])
+        
         y_t = np.array([item.detach().cpu().numpy() for sublist in yList for item in sublist])
         #print(f'Epoch: {epoch:03d}, Loss: {loss:.6f}, Train Acc: {train_acc:.4f}, Test Acc: {accuracy_score(y_t, y_p):.4f}, Test Bal Acc: {balanced_accuracy_score(y_t, y_p):.4f}')
 
         test_acc = accuracy_score(y_t, y_p)
         test_bal_acc = balanced_accuracy_score(y_t, y_p)
 
+        #print(test_acc, test_bal_acc, loss)
+
+        res_dict =  {"loss": loss, "train_acc": train_acc, "val_acc": test_acc, "val_bal_acc": test_bal_acc}
+
+        if not wandb.config.regression:
+            y_p_softmax = torch.nn.functional.softmax(torch.tensor(np.array(outList).squeeze()), dim = 1).detach().numpy()
+            if num_classes == 2:
+                mean_auc = roc_auc_score(
+                    y_true=y_t,
+                    y_score = y_p_softmax[:,1],
+                    multi_class="ovr",
+                    average="macro",
+                )
+            else:
+                mean_auc = roc_auc_score(
+                    y_true=y_t,
+                    y_score = y_p_softmax,
+                    multi_class="ovr",
+                    average="macro",
+                )
+                label_binarizer = LabelBinarizer().fit(y_t)
+                y_onehot_test = label_binarizer.transform(y_t)
+                # get auc for each class
+                fpr = dict()
+                tpr = dict()
+                roc_auc = dict()
+                for i in range(num_classes):
+                    fpr[i], tpr[i], _ = roc_curve(y_onehot_test[:, i], y_p_softmax[:, i])
+                    roc_auc[i] = auc(fpr[i], tpr[i])
+                    res_dict[f"roc_auc_{label_names[i]}"] = roc_auc[i]
+
+
+            if mean_auc > best_mean_auc or (mean_auc == best_mean_auc and test_bal_acc > best_val_bal_acc):
+                best_mean_auc = mean_auc
+                best_pred = y_p_softmax
+
+            #if best_mean_auc > th_swp:
+            #    torch.save(model.state_dict(), f'./checkpoints/model_{wandb.config.aggregation_mode}_best_auc.pt')
+
+            res_dict["mean_auc"] = mean_auc
+            res_dict["best_mean_auc"] = best_mean_auc
+
         if test_bal_acc > best_val_bal_acc:
             best_val_bal_acc = test_bal_acc
 
-        _, _, roc_auc = evaluation.roc_auc_multiclass(y_t, torch.nn.functional.softmax(torch.tensor(np.array(outList).squeeze()), dim = 1))
-        mean_auc = np.mean(list(roc_auc.values()))
-                           
-        if mean_auc > best_mean_auc or (mean_auc == best_mean_auc and test_bal_acc > best_val_bal_acc):
-            best_mean_auc = mean_auc
-            best_pred = outList
-
-            if best_mean_auc > th_swp:
-                torch.save(model.state_dict(), f'./checkpoints/model_{wandb.config.aggregation_mode}_best_auc.pt')
-             
-
-        res_dict =  {"loss": loss, "train_acc": train_acc, "val_acc": test_acc, "val_bal_acc": test_bal_acc, "best_val_bal_acc": best_val_bal_acc,"mean_auc" : mean_auc,"best_mean_auc": best_mean_auc}
+        kappa = cohen_kappa_score(y_t, y_p, weights= "quadratic")
 
 
-        for i in range(num_classes):
-            res_dict[f"roc_auc_{label_names[i]}"] = roc_auc[i]
+
+        res_dict["kappa"] = kappa
+        res_dict["best_val_bal_acc"]= best_val_bal_acc
 
         wandb.log(res_dict)
 
-    wandb.log({"roc": wandb.plot.roc_curve(y_t, torch.nn.functional.softmax(torch.tensor(np.array(best_pred).squeeze()), dim = 1),labels = label_names)})
+    if not wandb.config.regression:
+        wandb.log({"roc": wandb.plot.roc_curve(y_t, best_pred,labels = label_names)})
 
-    #roc_auc = roc_auc_score(y_t, torch.nn.functional.softmax(torch.tensor(np.array(best_pred).squeeze()), dim = 1), multi_class='ovr')
 
-    print(roc_auc)
-    for i in range(num_classes):
-        wandb.log({f"roc_auc_{label_names[i]}": roc_auc[i]})
 
 
 wandb.agent(sweep_id, function=main, count=100)
