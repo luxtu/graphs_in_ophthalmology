13cf55cb7fdac5291e03a09e0d180d209f566f27
Laurin <laurin.lux@tum.de>
2023-10-26 09:09:45 +0000

    pytorch checkpoints hash sum workaround

--- cnn_debug.py
+++ cnn_debug.py
@@ -1,4 +1,4 @@
-from torchvision.models import resnet18, ResNet18_Weights, efficientnet_b3, EfficientNet_B3_Weights, efficientnet_b2, EfficientNet_B2_Weights
+from torchvision.models import resnet18, ResNet18_Weights, efficientnet_b3, EfficientNet_B3_Weights, efficientnet_b2, EfficientNet_B2_Weights, efficientnet_b0, EfficientNet_B0_Weights
 from torchvision import transforms
 from torchvision.models._api import WeightsEnum
 from torch.hub import load_state_dict_from_url
@@ -12,31 +12,23 @@ from sklearn.metrics import accuracy_score, balanced_accuracy_score
 
 from evaluation import evaluation
 from utils import prep
-
+from types import SimpleNamespace
 import wandb
 
 # Define sweep config
-sweep_configuration = {
-    "method": "random",
-    "name": "cnn_sweep_pretrained_effnetb2",
-    "metric": {"goal": "maximize", "name": "best_mean_auc"},
-    "parameters": {
-        "model" : {"values": ["resnet", "effnet"]},
-        "batch_size": {"values": [8, 16, 32, 64]},
-        "epochs": {"values": [200]},
-        "lr": {"max": 0.005, "min": 0.0001}, # learning rate to high does not work
-        "weight_decay": {"max": 0.02, "min": 0.005},
-        "class_weights": {"values": ["unbalanced", "balanced"]}, # "balanced", 
-        "dataset": {"values": ["DCP"]}, #, "DCP"
-        "epochs": {"values": [100,200]},
-        "optimizer" : {"values": ["adam", "sgd"]},
-        "pretrained" : {"values": [False]},
-    },
+sweep_config_dict = {
+        "model" : "effnet",
+        "batch_size": 16,
+        "epochs": 100,
+        "lr": 0.0002, # learning rate to high does not work
+        "weight_decay": 0.01,
+        "class_weights":"unbalanced", # "balanced", 
+        "dataset": "DCP", #, "DCP"
+        "epochs": 100,
+        "optimizer" : "adam",
 }
 
-
-sweep_id = wandb.sweep(sweep=sweep_configuration, project="graph_pathology")
-#torch.cuda.empty_cache()
+sweep_config = SimpleNamespace(**sweep_config_dict)
 
 
 # Define your data transformations (modify as needed)
@@ -60,26 +52,26 @@ transform_train = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-#images = "/media/data/alex_johannes/octa_data/Cairo/SCP_images"
-images = "../dcp_images"
-#labels = "/media/data/alex_johannes/octa_data/Cairo/labels.csv"
-labels = "../labels.csv"
+images = "/media/data/alex_johannes/octa_data/Cairo/DCP_images"
+#images = "../dcp_images"
+labels = "/media/data/alex_johannes/octa_data/Cairo/labels.csv"
+#labels = "../labels.csv"
 
 
 train_dataset = image_loader.CNNImageLoader(path = images,
                                          label_file = labels,
                                          transform=transform_train,
-                                         mode = "train"
+                                         mode = "debug"
                                          )
 test_dataset = image_loader.CNNImageLoader(path = images,
                                          label_file = labels,
                                          transform=transform_test,
-                                         mode = "test"
+                                         mode = "debug"
                                          )
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 train_labels = [label for _, label in train_dataset]
-class_weights = prep.get_class_weights(train_labels, verbose = True)
+class_weights = prep.get_class_weights(train_labels, verbose = False)
 class_weights = torch.tensor(class_weights, device=device)
 class_weights = class_weights ** 0.5 
 
@@ -102,19 +94,23 @@ def xavier_init(model):
 
 
 
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+from torchvision.models._api import WeightsEnum
+from torch.hub import load_state_dict_from_url
+
+
+
 def main():
-    run = wandb.init()
 
-
-    if wandb.config.model == "resnet":
+    if sweep_config.model == "resnet":
         # Load the pre-trained ResNet18 model
-        weights = ResNet18_Weights.DEFAULT
-        model = resnet18(weights=weights)
+        #weights = ResNet18_Weights.DEFAULT
+        model = resnet18() #weights=weights)
         model.apply(xavier_init)
 
-    elif wandb.config.model == "effnet":
-        #weights = EfficientNet_B3_Weights.DEFAULT
-
+    elif sweep_config.model == "effnet":
+        #weights = EfficientNet_B2_Weights.DEFAULT
+        #model = efficientnet_b2(weights=weights)
         def get_state_dict(self, *args, **kwargs):
             #kwargs.pop("check_hash")
             return load_state_dict_from_url(self.url, *args, **kwargs)
@@ -122,9 +118,6 @@ def main():
         WeightsEnum.get_state_dict = get_state_dict
         #model = efficientnet_b2(weights=EfficientNet_B2_Weights.IMAGENET1K_V1)
         model = efficientnet_b2(weights="DEFAULT")
-
-
-        #model = efficientnet_b3() #weights=weights)
         #model.apply(xavier_init)
 
     ## Train all layers
@@ -135,10 +128,10 @@ def main():
 
     # Modify the last fully connected layer to match the number of classes in your task
 
-    if wandb.config.model == "resnet":
+    if sweep_config.model == "resnet":
         model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-    elif wandb.config.model == "effnet":
+    elif sweep_config.model == "effnet":
         model.classifier._modules['1'] = nn.Linear(model.classifier._modules['1'].in_features, num_classes)
     balanced_loss = torch.nn.CrossEntropyLoss(class_weights)
     unbalanced_loss = torch.nn.CrossEntropyLoss()
@@ -146,22 +139,22 @@ def main():
     loss_dict = {"balanced": balanced_loss, "unbalanced": unbalanced_loss}
 
     # Define your loss function and optimizer
-    criterion = loss_dict[wandb.config.class_weights]
+    criterion = loss_dict[sweep_config.class_weights]
 
-    optimizer_dict = {"adam": optim.Adam(model.parameters(), lr=wandb.config.lr, weight_decay=wandb.config.weight_decay),
-                        "sgd": optim.SGD(model.parameters(), lr=wandb.config.lr, momentum=0.9, weight_decay=wandb.config.weight_decay)}
+    optimizer_dict = {"adam": optim.Adam(model.parameters(), lr=sweep_config.lr, weight_decay=sweep_config.weight_decay),
+                        "sgd": optim.SGD(model.parameters(), lr=sweep_config.lr, momentum=0.9, weight_decay=sweep_config.weight_decay)}
 
-    optimizer = optimizer_dict[wandb.config.optimizer]
+    optimizer = optimizer_dict[sweep_config.optimizer]
 
     # Training loop
     model.to(device)
 
 
-    batch_size = wandb.config.batch_size  
+    batch_size = sweep_config.batch_size  
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False, num_workers=1)
 
-    for epoch in range(1, wandb.config.epochs +1):
+    for epoch in range(1, sweep_config.epochs +1):
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
 
@@ -193,18 +186,10 @@ def main():
         train_accuracy, train_balanced_accuracy, train_kappa, train_mean_auc  = evaluation.evaluate_cnn(model, train_loader, num_classes)
         test_accuracy, test_balanced_accuracy, test_kappa, test_mean_auc = evaluation.evaluate_cnn(model, test_loader, num_classes)
 
-        wandb.log({"loss": loss,
-                    "train_acc": train_accuracy, 
-                    "test_acc": test_accuracy, 
-                    "test_bal_acc": test_balanced_accuracy, 
-                    "train_kappa": train_kappa, 
-                    "test_kappa": test_kappa, 
-                    "train_mean_auc": train_mean_auc, 
-                    "test_mean_auc": test_mean_auc})
-
+        print("Epoch: {} \Loss: {:.6f}  \tTraining Accuracy: {:.3f} \tTraining Balanced Accuracy: {:.3f}".format(epoch, loss, train_accuracy, train_balanced_accuracy))
 
 
 # Save the fine-tuned model if needed
 #torch.save(resnet.state_dict(), 'fine_tuned_resnet18.pth')
 
-wandb.agent(sweep_id, function=main, count=100)
+main()
