0941a9ea05c35d92026bedf4c32451e4b06b0a3b
Laurin <laurin.lux@tum.de>
2023-11-02 10:35:17 +0000

    adaption for start and end layers

--- explainability/grad_cam.py
+++ explainability/grad_cam.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 
-def grad_cam_data(model, data, target_class, scale = False, relu = False):
+def grad_cam_data(model, data, target_class, scale = False, relu = False, start = False):
     # Perform forward pass for the target class
     model.eval()
     pos_dict = {}
@@ -21,7 +21,7 @@ def grad_cam_data(model, data, target_class, scale = False, relu = False):
     for c in target_class:
         output[:, c].backward(retain_graph=True)
         # Compute Grad-CAM
-        node_heat_map = grad_cam_heat_map(model, relu = relu) # .gnn1_embed
+        node_heat_map = grad_cam_heat_map(model, relu = relu, start = start) # .gnn1_embed
         heat_maps.append(node_heat_map)
 
     if scale:
@@ -54,20 +54,37 @@ def grad_cam_data(model, data, target_class, scale = False, relu = False):
         return heat_maps
 
 
-def grad_cam_heat_map(model, relu = False):
+def grad_cam_heat_map(model, start = False, relu = False):
     # Compute Grad-CAM
-    alphas_1 = torch.mean(model.final_conv_grads_1, axis=0)
-    alphas_2 = torch.mean(model.final_conv_grads_2, axis=0)
+
+    try:
+        model = model.gnn1_embed
+    except AttributeError:
+        pass
+
+    if start == True:
+        conv_grads_1 = model.start_conv_grads_1
+        conv_grads_2 = model.start_conv_grads_2
+        conv_acts_1 = model.start_conv_acts_1
+        conv_acts_2 = model.start_conv_acts_2
+    else:
+        conv_grads_1 = model.final_conv_grads_1
+        conv_grads_2 = model.final_conv_grads_2
+        conv_acts_1 = model.final_conv_acts_1
+        conv_acts_2 = model.final_conv_acts_2
+
+    alphas_1 = torch.mean(conv_grads_1, axis=0)
+    alphas_2 = torch.mean(conv_grads_2, axis=0)
     node_heat_map_1 = []
-    for n in range(model.final_conv_acts_1.shape[0]):
-        node_heat = alphas_1 @ model.final_conv_acts_1[n]#).item() F.relu(
+    for n in range(conv_acts_1.shape[0]):
+        node_heat = alphas_1 @ conv_acts_1[n]#).item() F.relu(
         if relu:
             node_heat = F.relu(node_heat)
         node_heat_map_1.append(node_heat.item())
 
     node_heat_map_2 = []
-    for n in range(model.final_conv_acts_2.shape[0]):
-        node_heat = alphas_2 @ model.final_conv_acts_2[n]#).item() F.relu(
+    for n in range(conv_acts_2.shape[0]):
+        node_heat = alphas_2 @ conv_acts_2[n]#).item() F.relu(
         if relu:
             node_heat = F.relu(node_heat)
 
