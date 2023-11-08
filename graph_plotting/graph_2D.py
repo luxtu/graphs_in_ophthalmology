1ae014bb941303567c0697461c72583e3a959549
Laurin <laurin.lux@tum.de>
2023-11-08 09:03:32 +0000

    plotting for faz node data

--- graph_plotting/graph_2D.py
+++ graph_plotting/graph_2D.py
@@ -98,6 +98,7 @@ class HeteroGraphPlotter2D():
 
     graph_1_name = "graph_1"
     graph_2_name = "graph_2"
+    graph_3_name = "faz"
 
     def set_val_range(self, upper_range, lower_range):
         self.upper_range = upper_range
@@ -162,8 +163,8 @@ class HeteroGraphPlotter2D():
                 upper_range = self.upper_range
                 lower_range = self.lower_range
 
-            sc1 = ax.scatter(het_graph1_pos[:,1], het_graph1_pos[:,0], s = 8, c = sft_max1, cmap = "Oranges", zorder = 2, vmin = lower_range, vmax = upper_range ) #,alpha = sft_max1
-            sc2 = ax.scatter(het_graph2_pos[:,1], het_graph2_pos[:,0], s = 12, c = sft_max2, cmap = "Oranges", zorder = 2, marker = "s" , vmin = lower_range, vmax = upper_range) #alpha = sft_max2 
+            sc1 = ax.scatter(het_graph1_pos[:,1], het_graph1_pos[:,0], s = 8, c = sft_max1, cmap = "Oranges", zorder = 2, alpha= 0.5, vmin = lower_range, vmax = upper_range ) #,alpha = sft_max1
+            sc2 = ax.scatter(het_graph2_pos[:,1], het_graph2_pos[:,0], s = 12, c = sft_max2, cmap = "Oranges", zorder = 2, alpha= 0.5, marker = "s" , vmin = lower_range, vmax = upper_range) #alpha = sft_max2 
 
             #[:,self.cls]
             #[:,self.cls]
@@ -179,4 +180,87 @@ class HeteroGraphPlotter2D():
             plt.savefig(path)
             plt.close()
 
+
+    def plot_graph_2D_faz(self, het_graph , edges= True, ax = None, pred_val_dict = None, path = None):
+        # create a function that returns a plot of the graph in 2D
+        # use the node positions and the edge indices to plot the graph
+
+
+        if ax is None:
+            fig, ax = plt.subplots()
+            side_l = 5.5
+            fig.set_figwidth(side_l)
+            fig.set_figheight(side_l)
+
+            plt.ylim(0,1200)
+            plt.xlim(0,1200)
+
+        else:
+            ax.set_ylim(0,1200)
+            ax.set_xlim(0,1200)
+            ax.set_yticklabels([])
+            ax.set_xticklabels([])
+
+        het_graph1_pos = het_graph[self.graph_1_name].pos.cpu().detach().numpy()
+        het_graph2_pos = het_graph[self.graph_2_name].pos.cpu().detach().numpy()
+        het_graph3_pos = het_graph[self.graph_3_name].pos.cpu().detach().numpy()
+
+        if edges:
+            for edge in het_graph[self.graph_1_name, 'to', self.graph_1_name].edge_index.cpu().detach().numpy().T:
+                ax.plot(het_graph1_pos[edge,1], het_graph1_pos[edge,0], c="blue",linewidth=1, alpha=0.5, zorder = 1)
+            for edge in het_graph[self.graph_2_name, 'to', self.graph_2_name].edge_index.cpu().detach().numpy().T:
+                ax.plot(het_graph2_pos[edge,1], het_graph2_pos[edge,0], c="red",linewidth=1, alpha=0.5, zorder = 1)
+            for edge in het_graph[self.graph_1_name, 'to', self.graph_2_name].edge_index.cpu().detach().numpy().T:
+                #create a line for each edge 
+                x_pos = (het_graph1_pos[edge[0],1], het_graph2_pos[edge[1],1])
+                y_pos = (het_graph1_pos[edge[0],0], het_graph2_pos[edge[1],0])
+#
+                ax.plot(x_pos, y_pos, linewidth=1, alpha=0.5, c= "black", zorder = 1)
+                #break
+
+            for edge in het_graph[self.graph_3_name, 'to', self.graph_2_name].edge_index.cpu().detach().numpy().T:
+                x_pos = (het_graph3_pos[edge[0],1], het_graph2_pos[edge[1],1])
+                y_pos = (het_graph3_pos[edge[0],0], het_graph2_pos[edge[1],0])
+#
+                ax.plot(x_pos, y_pos, linewidth=1, alpha=0.5, c= "yellow", zorder = 1)
+
+            for edge in het_graph[self.graph_1_name, 'to', self.graph_3_name].edge_index.cpu().detach().numpy().T:
+                x_pos = (het_graph1_pos[edge[0],1], het_graph3_pos[edge[1],1])
+                y_pos = (het_graph1_pos[edge[0],0], het_graph3_pos[edge[1],0])
+
+                ax.plot(x_pos, y_pos, linewidth=1, alpha=0.5, c= "purple", zorder = 1)
+        if pred_val_dict is not None:
+
+            #actually not softmaxed
+            sft_max1 = pred_val_dict[self.graph_1_name].cpu().detach()#, dim=1)#functional.softmax(
+            sft_max2 = pred_val_dict[self.graph_2_name].cpu().detach()#, dim=1)#functional.softmax(
+
+
+            #max_v = max(pred_val_dict[self.graph_1_name].cpu().detach()[:,self.cls].max(), pred_val_dict[self.graph_2_name].cpu().detach()[:,self.cls].max())#1
+            #min_v = min(pred_val_dict[self.graph_1_name].cpu().detach()[:,self.cls].min(), pred_val_dict[self.graph_2_name].cpu().detach()[:,self.cls].min())#0
+
+            #upper_range = max(max_v, abs(min_v))
+            if not hasattr(self, "upper_range"):
+                upper_range = max(sft_max1.max(), sft_max2.max())
+                lower_range = min(sft_max1.min(), sft_max2.min())
+            else:
+                upper_range = self.upper_range
+                lower_range = self.lower_range
+
+            sc1 = ax.scatter(het_graph1_pos[:,1], het_graph1_pos[:,0], s = 8, c = sft_max1, cmap = "Oranges", zorder = 2, alpha= 0.5, vmin = lower_range, vmax = upper_range ) #,alpha = sft_max1
+            sc2 = ax.scatter(het_graph2_pos[:,1], het_graph2_pos[:,0], s = 12, c = sft_max2, cmap = "Oranges", zorder = 2, alpha= 0.5, marker = "s" , vmin = lower_range, vmax = upper_range) #alpha = sft_max2 
+            sc3 = ax.scatter(het_graph3_pos[:,1], het_graph3_pos[:,0], s = 16, c = sft_max2, cmap = "Oranges", zorder = 2, alpha= 0.5, marker = "D" , vmin = lower_range, vmax = upper_range) #alpha = sft_max2
+            #[:,self.cls]
+            #[:,self.cls]
+
+            plt.colorbar(sc2)
+        else:
+            ax.scatter(het_graph1_pos[:,1], het_graph1_pos[:,0],alpha = 0, s = 8)
+            ax.scatter(het_graph2_pos[:,1], het_graph2_pos[:,0],alpha = 0, s = 12, marker = "s")
+            ax.scatter(het_graph3_pos[:,1], het_graph3_pos[:,0],alpha = 0, s = 16, marker = "D")
+
+        if path is not None:
+            plt.tight_layout()
+            plt.savefig(path)
+            plt.close()
 
