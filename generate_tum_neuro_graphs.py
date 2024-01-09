8fa98e9412be082348eefe7eaf87a37c615caaf4
laurin <laurin.lux@tum.de>
2024-01-09 17:15:12 +0100

    script to generate graphs for tumneuro

--- generate_tum_neuro_graphs.py
+++ generate_tum_neuro_graphs.py
@@ -3,17 +3,17 @@ import pandas as pd
 import os 
 
 
-datatype = "SVC"
+datatype = "DVC"
 
-segmentation_path = f"../../TumNeuro/checked_seg_{datatype}"
-json_path = f"../../TumNeuro/checked_seg_{datatype}_graph/json"
-void_graph_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph/void_graph_faz"
-hetero_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph/hetero_edges_faz"
-image_path = f"../../TumNeuro/checked_images_{datatype}/"
+segmentation_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/seg"
+json_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/json"
+void_graph_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/void_graph_faz"
+hetero_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/hetero_edges_faz"
+image_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/images"
 
-faz_node_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph/faz_node"
-faz_region_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph/faz_region_edges"
-faz_vessel_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph/faz_vessel_edges"
+faz_node_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/faz_node"
+faz_region_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/faz_region_edges"
+faz_vessel_edges_save_path = f"../../TumNeuro/checked_seg_{datatype}_graph_new_idx/faz_vessel_edges"
 
 Gernerator_from_VVG = heterograph_from_vvg_generator.HeterographFromVVGGenerator(
                                                             seg_path= segmentation_path,
